182bff43281622c27942e0ffaa44f273dd918616
Javi Palacios <javi@fjp.es>
2017-09-17 11:46:18 +0200

    Adding pagination to the home page and to the users public profiles

--- src/AppBundle/Controller/PublicController.php
+++ src/AppBundle/Controller/PublicController.php
@@ -16,17 +16,21 @@ use Symfony\Component\HttpFoundation\Request;
 class PublicController extends Controller
 {
     /**
-     * @Route("/", name="home", defaults={"_format"="html"})
-     * @Route("/rss/", name="rss", defaults={"_format"="xml"})
+     * @Route("/", name="home", defaults={"_format"="html", "page": "1"})
+     * @Route("/page/{page}", name="paginated_home", defaults={"_format"="html"}, requirements={"page": "[1-9]\d*"})
+     * @Route("/rss/", name="rss", defaults={"_format"="xml", "page": "1"})
      */
-    public function indexAction(Request $request, $_format)
+    public function indexAction(Request $request, $_format, $page)
     {
         $repository = $this->getDoctrine()->getRepository('AppBundle:Post');
-        $posts = $repository->findBy(array(
-                'status' => 'publish',
-                'type' => 'post'), array(
-                'date' => 'DESC'
-        ));
+        $posts = $repository->getPaginatedIndex($page);
+        $route = $request->attributes->get('_route');
+        $totalItems = count($posts);
+        if ($route == 'rss') {
+            $pagesCount = ceil($totalItems / Post::RSS_NUM_ITEMS);
+        } else {
+            $pagesCount = ceil($totalItems / Post::NUM_ITEMS);
+        }
         $locale = $request->getLocale();
         if ($locale == 'es') {
             $pages = $repository->findBy(array(
@@ -43,7 +47,10 @@ class PublicController extends Controller
         }
         return $this->render('public/index.'.$_format.'.twig', array(
                 'posts' => $posts,
-                'pages' => $pages
+                'pages' => $pages,
+                'totalItems' => $totalItems,
+                'pagesCount' => $pagesCount,
+                'page' => $page
         ));
     }
 
@@ -84,6 +91,8 @@ class PublicController extends Controller
                 'pages' => $pages,
                 'categories' => $categories
             ));
+        } elseif ($slug == 'rss') {
+            return $this->redirectToRoute('rss');
         } elseif ($slug == 'login') {
             return $this->redirectToRoute('login');
         } else {
@@ -96,18 +105,16 @@ class PublicController extends Controller
     }
 
     /**
-     * @Route("/profile/{username}", name="profile")
+     * @Route("/profile/{username}", name="profile", defaults={"page": "1"})
+     * @Route("/profile/{username}/page/{page}", name="paginated_profile", requirements={"page": "[1-9]\d*"})
      */
-    public function profileAction(Request $request, User $user)
+    public function profileAction(Request $request, User $user, $page)
     {
         $em = $this->getDoctrine()->getManager();
         $postRepo = $em->getRepository('AppBundle:Post');
-        $posts = $postRepo->findBy(array(
-            'author' => $user->getId(),
-            'type' => 'post',
-            'status' => 'publish'), array(
-            'date' => 'DESC'
-        ));
+        $posts = $postRepo->getPaginatedProfile($page, $user->getId(), 1);
+        $totalItems = count($posts);
+        $pagesCount = ceil($totalItems / Post::NUM_ITEMS);
         $locale = $request->getLocale();
         if ($locale == 'es') {
             $pages = $postRepo->findBy(array(
@@ -125,14 +132,19 @@ class PublicController extends Controller
         return $this->render('public/profile.html.twig', array(
             'posts' => $posts,
             'user' => $user,
-            'pages' => $pages
+            'pages' => $pages,
+            'totalItems' => $totalItems,
+            'pagesCount' => $pagesCount,
+            'page' => $page,
+            'username' => $user->getUsername()
         ));
     }
 
     /**
-     * @Route("/category/{slug}", name="category")
+     * @Route("/category/{slug}", name="category", defaults={"page": "1"})
+     * @Route("/category/{slug}/page/{page}", name="paginated_category", requirements={"page": "[1-9]\d*"})
      */
-    public function categoryAction(Request $request, Category $category)
+    public function categoryAction(Request $request, Category $category, $page)
     {
         $em = $this->getDoctrine()->getManager();
         $postRepo = $em->getRepository('AppBundle:Post');
--- src/AppBundle/Repository/PostRepository.php
+++ src/AppBundle/Repository/PostRepository.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\Post;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+
 /**
  * PostRepository
  *
@@ -10,4 +13,30 @@ namespace AppBundle\Repository;
  */
 class PostRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function getPaginatedIndex($currentPage = 1, $perPage = Post::NUM_ITEMS)
+    {
+        $em = $this->getEntityManager();
+        $dql = "SELECT p, c FROM AppBundle\Entity\Post p JOIN p.category c
+                WHERE p.type = 'post' AND p.status = 'publish'
+                ORDER BY p.date DESC";
+        $query = $em->createQuery($dql)
+            ->setFirstResult($perPage * ($currentPage - 1))
+            ->setMaxResults($perPage);
+        $paginator = new Paginator($query, $fetchJoinCollection = true);
+        return $paginator;
+    }
+
+    public function getPaginatedProfile($currentPage = 1, $userId, $perPage = Post::NUM_ITEMS)
+    {
+        $em = $this->getEntityManager();
+        $dql = "SELECT p, c FROM AppBundle\Entity\Post p JOIN p.category c
+                WHERE p.type = 'post' AND p.status = 'publish' AND p.author = :userId
+                ORDER BY p.date DESC";
+        $query = $em->createQuery($dql)
+            ->setParameter('userId', $userId)
+            ->setFirstResult($perPage * ($currentPage - 1))
+            ->setMaxResults($perPage);
+        $paginator = new Paginator($query, $fetchJoinCollection = true);
+        return $paginator;
+    }
 }
